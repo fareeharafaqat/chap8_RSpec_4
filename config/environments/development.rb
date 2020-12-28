b35ed20d983f96c15be3b34768b302cd3ea52bc7
fareeharafaqat <73890039+fareeharafaqat@users.noreply.github.com>
2020-12-28 13:34:19 +0500

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,5 +62,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+  config.hosts << /[a-z0-9]+\.educative\.run/ 
+
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
